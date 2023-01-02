a0a37733d46488fe60048a5f5576a5d2e6e7ab96
Konstantin <woodprop@yandex.ru>
2023-01-02 22:32:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+    const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+      'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = [null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const arr = number.toString().split('').map(n => +n);
+
+    if (number === 0) return 'zero';
+
+
+    let res = '';
+
+    if (arr.length === 3) {
+      res += digits[arr[0]] + ' hundred';
+      arr.shift();
+    }
+
+    if (arr.length === 2) {
+        if (arr[0] !== 0) {
+            if (res.length > 0) res += ' ';
+            if (+(arr.join('')) < 20) {
+                res += digits[+(arr.join(''))];
+                return res;
+            }
+            res += tens[arr[0]];
+        }
+        arr.shift();
+    }
+
+    if (arr.length === 1) {
+        if (arr[0] === 0) return res;
+        if (res.length > 0) res += ' ';
+
+        res += digits[arr[0]];
+    }
+
+  return res;
 }
